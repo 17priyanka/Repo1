26af1051d518e18d24b26b51e2d0b995f28832e7
17priyanka <151745888+17priyanka@users.noreply.github.com>
2023-11-29 12:16:46 +0530

    Add files via upload

---
+++ FinallyAssign.java
@@ -0,0 +1,28 @@
+////Writing a program in Java to finally
+package assignment;
+
+public class FinallyAssign {
+
+	
+	    
+	        public static void main(String[] args)
+	        {
+	            int a=45,b=0,rs=0;
+	            try
+	            {
+	                rs = a / b;
+	            }
+	            catch(ArithmeticException Ex)
+	            {
+	                System.out.print("\n\tError : " + Ex.getMessage());
+	            }
+	            finally
+	            {
+	                System.out.print("\n\tThe result is : " + rs);
+	            }
+	        }
+	    }
+
+	
+
+
